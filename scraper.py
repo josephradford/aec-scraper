d5bcf737346acdb05490eca0f733bb1e26f51199
Joseph Radford <joeradford@gmail.com>
2018-10-07 17:43:59 +1100

    Initial commit of entry function
    Formatted as per Jeff Knupp guide

---
+++ scraper.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""Script to save all of AEC donation data."""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
